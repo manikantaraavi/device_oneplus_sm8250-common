82901eee173b358cbb3842ee99faf73b6ca0e21e
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 14:00:16 -0800

    sm8250-common: Use 6GB dalvik-heap config
    We are currently using 2GB dalvik-heap config.
    
    Bug: 145823510
    Test: MPTS
    Change-Id: I086d4a5c09634c3c7bd51ec5d08dcaa1551511b4
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Signed-off-by: manikantaraavi <raavimanikanta6595@gmail.com>

--- common.mk
+++ common.mk
@@ -22,6 +22,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sm8250-common/sm8250-common-vendor.mk)
 
